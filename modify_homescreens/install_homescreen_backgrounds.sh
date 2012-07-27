154950a91cf7d11f633adddb6602d02a8a9793e2
Magnus Woldrich <m@japh.se>
2012-07-27 09:04:56 +0200

    add current filebase

---
+++ modify_homescreens/install_homescreen_backgrounds.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -i meegotouch-desktop-bg*.png /usr/share/themes/blanco/meegotouch/images/backgrounds/
+cp -i usr/share/themes/base/meegotouch/meegotouchhome/style/meegotouchhome.css \
+      /usr/share/themes/base/meegotouch/meegotouchhome/style/meegotouchhome.css
+echo "now, 'reboot' :)"
